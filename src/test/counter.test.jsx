c3866dbcb458b3ceb2c3e6c378333f6a0537ba3c
Axel Lindgren Wenell <atomwar.box@gmail.com>
2025-04-24 00:41:32 +0200

    added mockups

--- src/test/counter.test.jsx
+++ src/test/counter.test.jsx
@@ -1,5 +1,12 @@
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server.js";
+import "whatwg-fetch";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Counter from "../components/Counter/Counter.jsx";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 it("visar att count är 0 som standard", () => {
     render(<Counter />);
---
+++ src/test/mocks/handlers.js
@@ -0,0 +1,16 @@
+import { http } from "msw";
+
+export const handlers = [
+    http.post("https://tokenservice-jwt-2025.fly.dev/token-service/v1/request-token", async ({ request }) => {
+        const { username, password } = await request.json();
+
+        if(username === "axel" && password === "123456789") {
+            return new HttpResponse("mocked-jwt-token", {
+                status: 200,
+                headers: { "Content-Type": "text/plain" }
+            });
+        }
+
+        return HttpResponse.json({ message: "Unauthorized" }, { status: 401 });
+    })
+]
